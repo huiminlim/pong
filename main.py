99ae35f586c4ec844c39e7dc1d866d5cbcc5468f
huiminlim <e0036379@u.nus.edu>
2022-11-19 22:22:00 +0800

    Put pyxel canvas into class

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pyxel
 
-pyxel.init(160, 120)
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
 
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
-
-
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
-
-
-pyxel.run(update, draw)
+App()
